f817d1013ab697d671f085fe11fb92bc9b9277ff
TheROMZ52 <mahbodvismeh91@gmail.com>
2026-09-21 20:01:43 +0330

    Fix MySQL pool configuration

--- lib/db.ts
+++ lib/db.ts
@@ -4,11 +4,4 @@ const url = process.env.DATABASE_URL
 
 if (!url) throw new Error('DATABASE_URL is not configured')
 
-export const db = mysql.createPool({
-  uri: url,
-  waitForConnections: true,
-  connectionLimit: 10,
-  maxIdle: 10,
-  idleTimeout: 60000,
-  enableKeepAlive: true
-})
+export const db = mysql.createPool(url)
